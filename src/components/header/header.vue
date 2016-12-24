29b2496a781984e20824b26bc40e8a5352dd36fa
moling3650 <365024424@qq.com>
2016-12-24 11:43:20 +0800

    建立tab的基础模板

---
+++ src/components/header/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="header">头部</div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
