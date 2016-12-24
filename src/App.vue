d241bddad1888b25157c793398ea79577b5ea1fd
moling3650 <365024424@qq.com>
2016-12-24 14:01:58 +0800

    搭建tab的路由组件的框架

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="app">
+  <div>
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item">
+        <a v-link="{path: '/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/seller'}">商家</a>
+      </div>
     </div>
-    <div class="content">I am content</div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -20,13 +26,18 @@
 </script>
 
 <style  lang="stylus" rel="stylesheet/stylus">
-  #app
-    .tab
-      display: flex
-      width: 100%
-      height: 40px
-      line-height: 40px
-      .tab-item
-        flex: 1
-        text-align: center
+  .tab
+    display flex
+    width 100%
+    height 40px
+    line-height 40px
+    .tab-item
+      flex 1
+      text-align center
+      & > a
+        display block
+        font-size 14px
+        color rgb(77, 85, 93)
+        &.active
+          color rgb(240, 20, 20)
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import goods from 'components/goods/goods'
+import ratings from 'components/ratings/ratings'
+import seller from 'components/seller/seller'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+Vue.use(VueRouter)
+
+let app = Vue.extend(App)
+
+let router = new VueRouter({
+  linkActiveClass: 'active'
 })
+
+// 定义路由－组件映射
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/seller': {
+    component: seller
+  }
+})
+
+// 挂载路由
+router.start(app, '#app')
+
+// 跳转到/goods
+router.go('/goods')
