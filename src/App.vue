92db5a39efe899d2f0cdf7491353eeb5fa356c3d
moling3650 <365024424@qq.com>
2016-12-25 18:15:27 +0800

    导入vue-resource，模拟发送ajax获得数据

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,23 @@
 
 <script>
   import header from 'components/header/header.vue'
+
+  const ERR_OK = 0
+
   export default {
+    data () {
+      return {
+        seller: {}
+      }
+    },
+    created () {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.seller = response.data
+        }
+      })
+    },
     components: {
       'v-header': header
     }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App'
 import goods from 'components/goods/goods'
 import ratings from 'components/ratings/ratings'
@@ -8,6 +9,7 @@ import seller from 'components/seller/seller'
 import 'common/stylus/index.styl'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 let app = Vue.extend(App)
 
