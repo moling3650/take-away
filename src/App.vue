42ee2bdfdbc7f6ae19806684fb8e46251027d730
moling3650 <365024424@qq.com>
2016-12-25 17:20:01 +0800

    添加1px的border的通用写法

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <div class="tab-item">
+
         <a v-link="{path: '/goods'}">商品</a>
       </div>
       <div class="tab-item">
@@ -17,7 +18,7 @@
 </template>
 
 <script>
-  import header from './components/header/header.vue'
+  import header from 'components/header/header.vue'
   export default {
     components: {
       'v-header': header
@@ -26,11 +27,14 @@
 </script>
 
 <style  lang="stylus" rel="stylesheet/stylus">
+  @import 'common/stylus/mixin'
+
   .tab
     display flex
     width 100%
     height 40px
     line-height 40px
+    border-1px(rgba(7, 17, 27, 0.1))
     .tab-item
       flex 1
       text-align center
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import goods from 'components/goods/goods'
 import ratings from 'components/ratings/ratings'
 import seller from 'components/seller/seller'
 
+import 'common/stylus/index.styl'
+
 Vue.use(VueRouter)
 
 let app = Vue.extend(App)
