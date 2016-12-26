22681e5b707866ac2ceac9479aaa6472e71973fa
moling3650 <365024424@qq.com>
2016-12-26 19:52:11 +0800

    完成goods组件左侧菜单的基本布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper"></div>
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text border-1px">
+            <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
     <div class="foods-wrapper"></div>
   </div>
 </template>
@@ -20,6 +28,7 @@
       }
     },
     created () {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
       this.$http.get('/api/goods').then((response) => {
         response = response.body
         if (response.errno === ERR_OK) {
@@ -32,6 +41,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import '../../common/stylus/mixin'
   .goods
     display flex
     position absolute
@@ -43,6 +53,35 @@
       flex 0 0 80px
       width 80px
       background #f3f5f7
+      .menu-item
+        display table
+        width 56px
+        height 54px
+        line-height 14px
+        padding 0 12px
+        .icon
+          display inline-block
+          width 12px
+          height 12px
+          margin-right 2px
+          background-size 12px 12px
+          background-repeat no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display table-cell
+          width 56px
+          vertical-align middle
+          border-1px(rgba(7, 17, 27, 0.1))
+          font-size 12px
     .foods-wrapper
       flex 1
 </style>
