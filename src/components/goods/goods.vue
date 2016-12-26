2b39c972fd21bedba74c9e33a331ac984a50d6a1
moling3650 <365024424@qq.com>
2016-12-26 18:52:35 +0800

    编写商品组件的布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,48 @@
 <template>
-  <div>I am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  const ERR_OK = 0
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        goods: []
+      }
+    },
+    created () {
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.goods = response.data
+          console.dir(this.goods)
+        }
+      })
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .goods
+    display flex
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    overflow hidden
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background #f3f5f7
+    .foods-wrapper
+      flex 1
 </style>
