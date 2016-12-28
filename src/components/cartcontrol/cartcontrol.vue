dccc771bf2addd224250677783cd9f94585a59fe
moling3650 <365024424@qq.com>
2016-12-28 10:53:14 +0800

    实现cartcontrol组件的动画效果

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="cartcontrol">
-    <div v-show="food.count > 0" class="cart-decrease icon-remove_circle_outline" @click="decreaseCart($event)"></div>
+    <div v-show="food.count > 0" class="cart-decrease" @click="decreaseCart($event)" transition="move">
+      <span class="inner icon-remove_circle_outline"></span>
+    </div>
     <div v-show="food.count > 0" class="cart-count">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click="addCart($event)"></div>
   </div>
@@ -39,12 +41,25 @@
 <style lang="stylus" rel="stylesheet/stylus">
   .cartcontrol
     font-size 0
-    .cart-decrease, .cart-add
+    .cart-decrease
       display inline-block
       padding 6px
-      line-height 24px
-      font-size: 24px
-      color rgb(0, 160, 220)
+      transition all 0.4s linear
+      &.move-transition
+        opacity: 1
+        transform translate3D(0, 0, 0)
+        .inner
+          display inline-block
+          line-height: 24px
+          font-size: 24px
+          color rgb(0, 160, 220)
+          transition all 0.4s linear
+          transform rotate(0)
+      &.move-enter, &.move-leave
+        opacity: 0
+        transform translate3D(24px, 0, 0)
+        .inner
+          transform rotate(180deg)
     .cart-count
       display inline-block
       vertical-align top
@@ -54,6 +69,12 @@
       text-align center
       font-size 10px
       color rgb(147, 153, 159)
+    .cart-add
+      display inline-block
+      padding 6px
+      line-height 24px
+      font-size: 24px
+      color rgb(0, 160, 220)
 
 
 
