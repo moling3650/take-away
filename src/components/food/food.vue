6bed937f4413a8f3846177b5168a403d1f7acdbb
moling3650 <365024424@qq.com>
2016-12-29 09:51:07 +0800

    添加food组件

---
+++ src/components/food/food.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-show="foodShow" class="food"></div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    methods: {
+      show () {
+        this.foodShow = true
+      }
+    },
+    data () {
+      return {
+        foodShow: false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .food
+    position fixed
+    top 0
+    left 0
+    bottom 48px
+    z-index 30
+    width 100%
+    background #fff
+</style>
