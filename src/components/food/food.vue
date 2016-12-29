6964d0636d30db0a7d4cb34a115409f94f0508f3
moling3650 <365024424@qq.com>
2016-12-29 11:00:26 +0800

    添加food组件的头部样式

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,8 +1,28 @@
 <template>
-  <div v-show="foodShow" class="food"></div>
+  <div v-show="foodShow" class="food" v-el:food transition="move">
+    <div class="food-content">
+      <div class="image-header">
+        <img :src="food.image" alt="food image" />
+        <div class="back" @click="hide">
+          <i class="icon-arrow_lift"></i>
+        </div>
+      </div>
+      <div class="content">
+        <h1 class="title">{{food.name}}</h1>
+        <div class="detail">
+          <span class="sell-count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
+        </div>
+        <div class="price">
+          <span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+
   export default {
     props: {
       food: {
@@ -12,6 +32,18 @@
     methods: {
       show () {
         this.foodShow = true
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$els.food, {
+              click: true
+            })
+          } else {
+            this.scroll.refresh()
+          }
+        })
+      },
+      hide () {
+        this.foodShow = false
       }
     },
     data () {
@@ -31,4 +63,57 @@
     z-index 30
     width 100%
     background #fff
+    &.move-transition
+      transition all 0.2s linear
+    &.move-enter, &.move-leave
+      transform translate3d(100%, 0, 0)
+    .image-header
+      position relative
+      width 100%
+      height 0
+      padding-top 100%
+      img
+        position absolute
+        top 0
+        left 0
+        width 100%
+        height 100%
+      .back
+        position absolute
+        top 10px
+        left 0
+        .icon-arrow_lift
+          display block
+          padding 10px
+          font-size 20px
+          color #fff
+    .content
+      padding 18px
+      .title
+        line-height 14px
+        margin-bottom 8px
+        font-size 14px
+        font-weight 700
+        color rgb(7, 17, 27)
+      .detail
+        margin-bottom 18px
+        line-height 10px
+        height 10px
+        font-size 0
+        .sell-count, .rating
+          font-size 10px
+          color rgb(147, 153, 159)
+        .sell-count
+          margin-right 12px
+      .price
+        font-weight 700
+        line-height 24px
+        .now
+          margin-right 8px
+          font-size 14px
+          color rgb(240, 20, 20)
+        .old
+          text-decoration line-through
+          font-size 10px
+          color rgb(147, 153, 159)
 </style>
