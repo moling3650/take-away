b1baec73a35fc3ae033f14a853a6aee33a2afe84
moling3650 <365024424@qq.com>
2017-01-01 22:14:35 +0800

    添加icon组件

---
+++ src/components/icon/icon.vue
@@ -0,0 +1,13 @@
+<template>
+    <span class="icon"></span>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .icon
+    display inline-block
+    background-repeat no-repeat
+</style>
